2de2374bfd1ccd745a1bdeb4d223903dd2a90374
Carlos Roberto de Oliveira Maciel <carlos.maciel@trescon.com.br>
2024-09-13 14:23:15 -0300

    ajuste enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyCec9zgk2tVP-ppOqd43D8d1tpFM6x4-pk",
-    authDomain: "solicitou-desenvolvimento.firebaseapp.com",
-    projectId: "solicitou-desenvolvimento",
-    storageBucket: "solicitou-desenvolvimento.appspot.com",
-    messagingSenderId: "537909212574",
-    appId: "1:537909212574:web:863a223b354313a9dc128f",
-    measurementId: "G-G2N04R5E61"
+    apiKey: "AIzaSyC1MSvDCM7GOGUObME8ytDbkLdaRQwc17c",
+    authDomain: "solicitou-producao.firebaseapp.com",
+    projectId: "solicitou-producao",
+    storageBucket: "solicitou-producao.appspot.com",
+    messagingSenderId: "476531413733",
+    appId: "1:476531413733:web:e34803d788dd312f1f7a0c",
+    measurementId: "G-1HBGZCWY1G"
   },
   production: true,
   hmr: false
